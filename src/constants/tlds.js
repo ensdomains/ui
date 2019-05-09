456d186197926d571c7058509e629dddb52b76c2
Jeff Lau <jeff-lau@live.com>
2019-05-09 16:17:36 +0800

    WIP

---
+++ src/constants/tlds.js
@@ -0,0 +1,17 @@
+export default {
+  eth: {
+    supported: true
+  },
+  xyz: {
+    supported: true
+  },
+  test: {
+    supported: true
+  },
+  reverse: {
+    supported: true
+  },
+  luxe: {
+    supported: true
+  }
+}
