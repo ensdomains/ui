2536e7b559324143d87cfa32124a84dd7878bfee
Makoto Inoue <2630+makoto@users.noreply.github.com>
2021-01-25 18:28:40 +0000

    Catch insufficient gas (#101)

--- src/registrar.js
+++ src/registrar.js
@@ -403,6 +403,20 @@ export default class Registrar {
     }
   }
 
+  async estimateGasLimit( cb ){
+    let gas = 0
+    try{
+      gas = (await cb()).toNumber()
+    }catch(e){
+      let matched = e.message.match(/err: insufficient funds for transfer \(supplied gas (.*)\)/)
+      if(matched){
+        gas = parseInt(matched[1])
+      }
+      console.log({gas, e, matched})
+    }
+    return gas + transferGasCost
+  }
+
   async renew(label, duration) {
     const permanentRegistrarControllerWithoutSigner = this
       .permanentRegistrarController
@@ -412,8 +426,9 @@ export default class Registrar {
     )
     const price = await this.getRentPrice(label, duration)
     const priceWithBuffer = getBufferedPrice(price)
-    const gas = await permanentRegistrarController.estimate.renew(label, duration, { value: priceWithBuffer})
-    const gasLimit = gas.toNumber() + transferGasCost
+    const gasLimit = await this.estimateGasLimit(() => {
+      return permanentRegistrarController.estimate.renew(label, duration, { value:priceWithBuffer})
+    })
     return permanentRegistrarController.renew(label, duration, { value: priceWithBuffer, gasLimit })
   }
 
@@ -426,8 +441,9 @@ export default class Registrar {
     )
     const prices = await this.getRentPrices(labels, duration)
     const pricesWithBuffer = getBufferedPrice(prices)
-    const gas = await bulkRenewal.estimate.renewAll(labels, duration, { value: pricesWithBuffer })
-    const gasLimit = gas.toNumber() + transferGasCost
+    const gasLimit = await this.estimateGasLimit(() => {
+      return bulkRenewal.estimate.renewAll(labels, duration, { value:pricesWithBuffer})
+    })
     return bulkRenewal.renewAll(
       labels,
       duration,
